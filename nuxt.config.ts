6cb30f4dd12a27b2672216d450a0eed9beb0468f
violoctecia <violoctecia@gmail.com>
2024-05-25 02:16:26 +0300

    new config, changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    devtools: { enabled: true },
-    css: ["@/assets/base.css"],
-    ssr: true,
-    nitro: {
-        preset: 'node-server'
+  devtools: { enabled: true },
+  css: ["@/assets/base.css"],
+  ssr: true,
+  app: {
+    head: {
+      link: [{ rel: "icon", type: "image/png", href: "/favicon.png" }],
     },
-    app: {
-        head: {
-            link: [
-                { rel: 'icon', type: 'image/png', href: '/favicon.png' }
-            ]
-        }
-    }
+  },
 });
